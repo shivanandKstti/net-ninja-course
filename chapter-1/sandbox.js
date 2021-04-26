2be68f9c758a6b3d68e19c1db0ba360519486ca7
yasharth <yash@simplify3x.com>
2021-04-26 16:21:21 +0530

    basic js

---
+++ chapter-1/sandbox.js
@@ -0,0 +1,139 @@
+// console.log('message');
+// console.log('message');
+
+// let age = 44;
+// let a = 2010;
+
+// console.log(age, a);
+
+// age = 30;
+// console.log(age);
+
+// const points = 33;
+// console.log(points)
+// points = 33;
+
+/**
+ * strings
+ */
+
+// strings
+
+// console.log('hello world');
+// let email = 'shiva@gmail.com';
+// console.log(email);
+
+// // string concatenation
+
+// let firstName = 'shiva';
+// let lastName = 'katti';
+
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+// console.log(fullName[0]);
+// console.log(fullName.length);
+// console.log(fullName.toUpperCase());
+// console.log(fullName.toLowerCase());
+// // console.log(fullName);
+// let index = email.indexOf('@');
+// console.log(index);
+
+// let email = 'shiva@gmail.com';
+
+// let result = email.lastIndexOf('g');
+// let result = email.slice(0, 5);
+// let result = email.substr(4, 10);
+// let result = email.replace('s', 'ss');
+// console.log(result);
+
+// let ninjas = [
+//     'one',
+//     'two',
+//     'three'
+// ];
+
+// console.log(ninjas);
+// console.log(ninjas[1]);
+
+// let ages = [23, 25, 30, 35];
+// console.log(ages);
+// console.log(ages[2]);
+// console.log(ages.length);
+
+// let result = ninjas.join(',');
+// let result = ninjas.indexOf('three');
+// let result = ninjas.concat(['items', 'four']);
+// let result = ninjas.push('value');
+// let result = ninjas.pop();
+// console.log(ninjas);
+
+// // let age;
+// let age = null;
+
+// console.log(age, age + 3, `the age is ${age}`)
+
+// const calcArea = function(radius) {
+//     // let area = 3.24 * radius ** 2;
+//     // return area;
+//     return 3.24 * radius ** 2;
+//     // console.log(area);
+// }
+
+// const area = calcArea(5);
+// console.log(area);
+
+
+// const calcArea = (radius) => { 
+//     return 3.14 * radius ** 2;
+// }
+
+// const calcArea = radius => {
+//     return 3.14 * radius ** 2;
+// }
+
+// const calcArea = (radius) => 3.14 * radius ** 2;
+// const area = calcArea(10);
+// console.log(area);
+
+// const blogs = [
+//     { title: 'blog one', likes: 20 }, { title: 'blog two', likes: 30 }
+// ]
+// let user = {
+//     name: 'shiva',
+//     age: 29,
+//     email: 'shiva@gmail.com',
+//     location: 'banglore',
+//     blogs: [{ title: 'blog one', likes: 20 }, { title: 'blog two', likes: 30 }],
+//     login: function() {
+//         console.log('user logged in');
+//     },
+//     logblogs: function() {
+//         // console.log(blogs);
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title, blog.likes);
+//         })
+//     }
+// };
+
+// console.log(user);
+// console.log(user.name);
+
+// user.age = 30;
+// console.log(user.age);
+
+// user.login();
+// user.logblogs();
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+// const area = 7.7;
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
+
+// const random = Math.random();
+// console.log(random);
+
+// console.log(Math.round(random * 100))
